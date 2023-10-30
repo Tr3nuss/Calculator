3bf6cb16a076993aab50ec36da35d3f66f86f653
Tr3nuss <ivashkaa2006@mail.ru>
2023-10-31 00:15:53 +0300

    shortening of the js code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //variables
+
 const display = document.querySelector('.display-board');
 const buttons = document.querySelectorAll('.calc-item');
 const mathButtons = document.querySelectorAll('.calc-math-item');
@@ -32,101 +33,71 @@ const mathButtons = document.querySelectorAll('.calc-math-item');
 
     mathButtons.forEach((item) => {
         item.addEventListener('click', (e) => {
-            let itemSymbol = e.target.textContent;
-
-            //calling functions 
-
-            if(itemSymbol == 'log') {
-                log();
-            }
-
-            if(itemSymbol == 'x²') {
-                pow();
-            }
-
-            if(itemSymbol == 'rnd') {
-                round();
-            }
-
-            if(itemSymbol == '√') {
-                sqrt();
-            }
-
-            if(itemSymbol == 'n2') {
-                parce2();
-            }
-
-            if(itemSymbol == 'sin') {
-                sin();
-            }
-
-            if(itemSymbol == 'n8') {
-                parce8();
-            }
-
-            if(itemSymbol == 'cos') {
-                cos();
-            }
-
-            if(itemSymbol == 'n16') {
-                parce16();
-            }
-
-            if(itemSymbol == 'tan') {
-                tan();
-            }
-
+            let itemSymbol = e.target.textContent;   
             
+            
+            switch(itemSymbol) {
+                case 'log':
+                    log();
+                    break;
 
+                case 'x²':
+                    pow();
+                    break;
+                
+                case 'rnd':
+                    round();
+
+                case '√':
+                    sqrt();
+                    break;
+
+                case 'n2':
+                    parce2();
+                    break;
+                    
+                case 'sin':
+                    sin();
+                    break;
+
+                case 'n8':
+                    parce8();
+                    break;
+                    
+                case 'cos':
+                    cos();
+                    break;
+                    
+                case 'n16':  
+                    parce16();
+                    break;
+                    
+                case 'tan':
+                    tan(); 
+                    break;   
+            }
+                                         
         });
     });
 
     //writing functions
 
+    const log = () => display.value = Math.log(display.value);  
 
-    const log = () => {
-        display.value = Math.log(display.value);
-    }
+    const pow = () => display.value = Math.pow(display.value, 2);
 
-    const pow = () => {
-        display.value = Math.pow(display.value, 2);
-    }
+    const round = () => display.value = Math.round(display.value);
 
-    const round = () => {
-        display.value = Math.round(display.value);
-    } 
+    const sqrt = () => display.value = Math.sqrt(display.value, 2);
 
-    const sqrt = () => {
-        display.value = Math.sqrt(display.value, 2);
-    }
+    const parce2 = () => display.value = (+display.value).toString(2);
 
-    const parce2 = () => {
-        display.value = (+display.value).toString(2);
-    }
-
-    const sin = () => {
-        display.value = Math.sin(display.value);
-    }
+    const sin = () => display.value = Math.sin(display.value) * 52;
                
-    const parce8 = () => {
-        display.value = (+display.value).toString(8);
-    }
+    const parce8 = () => display.value = (+display.value).toString(8);
 
-    const cos = () => {
-        display.value = Math.cos(display.value);
-    }
+    const cos = () => display.value = Math.cos(display.value) * 52;
 
-    const parce16 = () => {
-        display.value = (+display.value).toString(16);
-    }
+    const parce16 = () => display.value = (+display.value).toString(16);
 
-    const tan = () => {
-        display.value = Math.tan(display.value);
-    }
-   
-    
-    
-
-
-
-   
+    const tan = () => display.value = Math.tan(display.value) * 52;
